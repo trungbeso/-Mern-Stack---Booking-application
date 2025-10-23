e15edb9766d20cc63d3dbbc1f505148528f8a9b3
benjamin <benjamin@gmail.com>
2025-10-23 17:23:24 +0700

    feat: implement hotel addition functionality with form validation and routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,8 +8,12 @@ import "./index.css";
 import Layout from "./layouts/Layout";
 import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
+import AddHotel from "./pages/AddHotel";
+import { useAppContext } from "./contexts/AppContext";
 
 function App() {
+  const isLoggedIn = useAppContext();
+
   return (
     <Router>
       <Routes>
@@ -45,6 +49,19 @@ function App() {
             </Layout>
           }
         />
+
+        {isLoggedIn && (
+          <>
+            <Route
+              path="/add-hotel"
+              element={
+                <Layout>
+                  <AddHotel />
+                </Layout>
+              }
+            />
+          </>
+        )}
 
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
---
+++ client/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -0,0 +1,29 @@
+import { useForm, FormProvider } from "react-hook-form";
+import DetailSection from "./DetailSection";
+
+export type HotelFormData = {
+  name: string;
+  city: string;
+  country: string;
+  description: string;
+  type: string;
+  pricePerNight: number;
+  starRating: number;
+  facilities: string[];
+  imageFiles: FileList;
+  adultCount: number;
+  childCount: number;
+};
+
+const ManageHotelForm = () => {
+  const formMethods = useForm<HotelFormData>();
+  return (
+    <FormProvider {...formMethods}>
+      <form>
+        <DetailSection />
+      </form>
+    </FormProvider>
+  );
+};
+
+export default ManageHotelForm;
